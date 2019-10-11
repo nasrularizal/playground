3251b23c96e115f2beb3d6aa4f95a22268bf12ff
Nasrul Abdullah Rizal <nasrul.ariza@gmail.com>
2019-10-11 09:13:18 +0700

    Mission 3 Tambahan
    Send User Notification

---
+++ app/Listeners/SendUserContactFormNotification.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\ContactFormSubmitted;
+use App\Notifications\UserContactFormSubmitted;
+use App\User;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class SendUserContactFormNotification
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  ContactFormSubmitted  $event
+     * @return void
+     */
+    public function handle(ContactFormSubmitted $event)
+    {
+        $dataUser = $this->fillUser($event);
+        $user = new User();
+        $user->forceFill($dataUser)->notify(new UserContactFormSubmitted($event->contactForm));
+    }
+
+    public function fillUser($event)
+    {
+        return [
+            'name' => $event->name,
+            'email' => $event->email,
+        ];
+    }
+}
